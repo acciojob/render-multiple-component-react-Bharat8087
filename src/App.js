13af4d1c9daceae61c25de26a21ffba44de875e7
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 13:26:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ const projects = [
 function App() {
   return (
     <div className="ns-wrapper">
+      {/* Iterate over the projects array */}
       {projects.map((project, index) => (
         <div key={index}>
+          {/* Render the project name with data-ns-test attribute */}
           <h1 data-ns-test="project-name">{project.name}</h1>
+          {/* Render the project description with data-ns-test attribute */}
           <h6 data-ns-test="project-description">{project.description}</h6>
         </div>
       ))}
