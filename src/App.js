e795863ac0d7022a7aae891d005eff2ff2e76228
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 13:44:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ function App() {
     <div className="ns-wrapper">
       {projects.map((project, index) => (
         <div key={index}>
-          <h1 data-ns-test="project-name">{project.name}</h1>
-          <h6 data-ns-test="project-description">{project.description}</h6>
+          {/* Remove curly braces and quotation marks from data-ns-test attribute values */}
+          <h1 data-ns-test = "project-name">{project.name}</h1>
+          <h6 data-ns-tes t= "project-description">{project.description}</h6>
         </div>
       ))}
     </div>
