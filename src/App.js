c928e377ca7d8d3da55dc2a6380e302454928ba8
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 13:22:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <div className="ns-wrapper">
       {projects.map((project, index) => (
         <div key={index}>
-          <h1 [data-ns-test="project-name"]>{project.name}</h1>
-          <h6 [data-ns-test="project-description"]>{project.description}</h6>
+          <h1 data-ns-test="project-name">{project.name}</h1>
+          <h6 data-ns-test="project-description">{project.description}</h6>
         </div>
       ))}
     </div>
